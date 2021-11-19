9dbb00b08b3018c5c89fddffc9c9bcd908eddba3
ebertmx <87283949+ebertmx@users.noreply.github.com>
2021-11-18 17:16:47 -0800

    commented HUE embedded

--- HUE_embedded/HUE_embedded.ino
+++ HUE_embedded/HUE_embedded.ino
@@ -1,6 +1,14 @@
-#include <AccelStepper.h>
-#include <math.h>
+//** This program handle interaction between the HUE system and a server providing serial data.
+// Currently: Given a 20 byte serial string ended with a \n and consisting of letters and integers,
+//  this code will convert the integer values into positional instruction for the Nema 17 stepper
+//  motors contained in the HUE system.
+//**Created By: Matthew Ebert and Jamieson Fregeau
+//**Date: 2021-11-02
 
+#include <AccelStepper.h> // library used for making the AccellStepper objects.
+#include <math.h>         // needed for the voltage2angle conversion math.
+
+//These pins are connected from the arduino to the motor drivers.
 #define MOTOR_X_ENABLE_PIN 8
 #define MOTOR_X_STEP_PIN 2
 #define MOTOR_X_DIR_PIN 3
@@ -13,50 +21,47 @@
 #define MOTOR_Z_STEP_PIN 6
 #define MOTOR_Z_DIR_PIN 7
 
-#define p 1000  //precision of conversion mathmatics
+//precision of mathmatics used in the voltage2angle function.
+#define p 1000
 
+//These constants determine the pins to which the positional sensors must be connected.
 #define prA A7
 #define prB A6
 #define prC A5
 
-//constants
-const int rA_max = 1024;
-const int rA_min = 0;
-const int rB_max = 1024;
-const int rB_min = 0;
-const int rC_max = 1024;
+//Constants which define the range of motion of HUE and BERT.
+//These define the maximum analog voltage reading of the arduino.
+const int rA_max = 1019;
+const int rA_min = 5;
+const int rB_max = 1019;
+const int rB_min = 5;
+const int rC_max = 1019;
 const int rC_min = 0;
 
-
+// These define the maximum relative angles which the HUE frame can take (in radians)
+//  multiplied by the precision factor and stored as integers.
 const int Q1_max = 2.41 * p;
 const int Q1_min = 0.26 * p;
 const int Q2_max = 2.63 * p;
 const int Q2_min = 0.35 * p;
-const int Q3_max = 3.1415 * p;
-const int Q3_min = 3.1415 * p;
+const int Q3_max = PI * p;
+const int Q3_min = -PI * p;
 
-//const int S1_max =
 
-//constants for voltage2angle
+// Constants for voltage2angle.
+// These value define HUE and BERTS mechanical frame dimensions
 const  int OG = 0.0800 * p; //m*p
-//const int OG2 = pow(OG, 2);
 const  int L1 = 0.255 * p; //m
-//const  int L12 = L1 * L1;
 const  int CL1 = 0.14142136 * p; //m
-//const  int CL12 = pow(CL1, 2);
 const  int L1m = 0.1100 * p; //m
-//const  int L1m2 = L1m * L1m;
 const  int L2 = 0.2600 * p; //m
-//const int L22 = L2 * L2;
 const  int CL2 = 0.28044607 * p; //m
-//const  int CL22 = CL2 * CL2;
 const int L2e = 0.0800 * p; //m
-//const  int L2e2 = L2e * L2e;
 const  int OL = 0.109337002 * p; //m
-//const  int OL2 = OL * OL;
 const  int OX = 0.1055 * p; //m
 
-//Variables
+// Varialbes used to set the maximum speed and acceleration rates of HUE's stepper motors.
+// Initial value set here
 int alpha_speed = 100;
 int beta_speed = 100;
 int charlie_speed = 100;
@@ -64,54 +69,65 @@ int alpha_acc = 300;
 int beta_acc = 300;
 int charlie_acc = 500;
 
-int a_s = 0;
-int b_s = 0;
-int c_s = 0;
-
-int rA;
-int rB;
-int rC;
-
+// Variable used to store the step targets of the HUE's stepper motors (alpha->1, beta->2, charlie->3)
 int S1;
 int S2;
 int S3;
 
-unsigned int Q1 = Q1_min;
-unsigned int Q2 = Q2_min;
-unsigned int Q3 = 0;
-
-
-
-//serial variables
-String s_input = "";
-bool input_complete = false;
-int check = 1;
-//Steppers
+//Stepper object definitions
 AccelStepper alpha(1, MOTOR_X_STEP_PIN, MOTOR_X_DIR_PIN);
 AccelStepper beta(1, MOTOR_Y_STEP_PIN, MOTOR_Y_DIR_PIN);
 AccelStepper charlie(1, MOTOR_Z_STEP_PIN, MOTOR_Z_DIR_PIN);
 
-
+// The string used to collect recieved serial input.
+String s_input = "";
 
 //functions
-//-----------------------------------------------------
-int voltage2angle(int qin1, int qin2, int qin3)
-{
-  //Serial.println("converting voltage value to step angle...");
-  //convert to decimal precision int
-  double Q1 = 0.26 + qin1 * 0.0020996;
-  double Q2 = 0.35 + qin2 * 0.002226;
-  double Q3 = -PI + qin3 * 2 * PI / 1024;
+//------------------------------------------------------------------------------------------------
 
-  //double now = micros();
-  //stepper 1
+
+int voltage2angle(int a_in, int b_in, int c_in)
+{
+  // This function converts a analog voltage value (0 to 1024) to a step position.
+  // This function relies heavily on the defined dimensions of HUE and BERT.
+
+  // This fuction will confirm the validity of the sent values against the
+  //  defined limits of HUE's mechanical frame.
+
+  // This function generally take 2ms to run.
+  // Avoid the use of delays or print function to speed up compeletion.
+
+  //Currently, doubles and long types are used to complete the mathmatical conversion.
+  //For security and speed reason these should be changed in future version to ints.
+
+  //Serial.println("converting voltage value to step angle...");
+
+  //Convert the analog reading into the cooresponding arm angle in radians.
+  double Q1 = 0.26 + a_in * 0.0020996;  //CHANGE THIS IF DIMENSION OF POT VALUES CHANGE*********
+  double Q2 = 0.35 + b_in * 0.002226;  //CHANGE THIS IF DIMENSION OF POT VALUES CHANGE***********
+  double Q3 = -PI + c_in * 2 * PI / 1024; //CHANGE THIS IF DIMENSION OF POT VALUES CHANGE********
+  //check if the desired value is valid
+
+
+  if (!checkangles(Q1, Q2, Q3))
+  {
+    //if angle outside limits, return false and report back to main
+    return 0;
+  }
+
+  //double now = micros(); //Uncomment this to start timing function
+
+
+  //Convert arm angle to motor shaft angle for stepper alpha
+  //See math model for more information on equation here
   double q1_theta = PI   - Q1;
   long O_L1m = sqrt(pow(L1m, 2) + pow(OL, 2) - 2 * L1m * OL * cos(q1_theta));
   double q1_beta =  acos(( pow(OG, 2)  - pow(CL1, 2) + pow(O_L1m, 2)) / (2 * OG * O_L1m));
   double q1_alpha = acos( (pow(OL, 2)  - pow(L1m, 2) + pow(O_L1m, 2)) / (2 * OL * O_L1m ));
   double qS1 = (q1_beta + q1_alpha); //Angle of OG1 referenced to the line between center of planetary gear and pivot of L1
 
-  //stepper 2
+  //Convert arm angle to motor shaft angle for stepper beta
+  //See math model for more information on equation here
   double q2_theta = PI - Q2;
   long R2 = sqrt(pow(L1, 2) + pow(L2e, 2) - 2 * long(L1) * long(L2e) * cos(q2_theta));
   double q_L1_R2 = acos((-pow(L2e, 2)  + pow(R2, 2) + pow(L1, 2)) / (2 * R2 * L1));
@@ -121,12 +137,19 @@ int voltage2angle(int qin1, int qin2, int qin3)
   double q2_alpha =  acos((pow(OL, 2) - pow(R2, 2) + pow(R1, 2)) / (2 * OL * R1));
   double qS2 = q2_alpha + q2_beta;//Angle of OG2 referenced to the line between center of planetary gear and pivot of L1
 
-  S1 = (3.442 - qS1) / (2 * PI) * 4 * 200;
-  S2 = (4.114 - qS2) / (2 * PI) * 4 * 200;
-  S3 = (Q3 * 13) * 200 / (2 * PI);
-  // double then = micros();
+  //Convert motor arm angle to steps based on a precision of 200 steps per rotation
+  S1 = (3.442 - qS1) / (2 * PI) * 4 * 200; //CHANGE THIS IF alpha STEPPER CHANGES*******
+  S2 = (4.114 - qS2) / (2 * PI) * 4 * 200; //CHANGE THIS IF beta STEPPER CHANGES********
+  S3 = (Q3 * 13) * 200 / (2 * PI);// CHANGE THIS IF charlie STEPPER CHANGES*************
 
 
+  // double then = micros();//Uncomment to end timing function
+
+  //  Serial.print("            time it took = ");
+  //  Serial.println(then - now);
+  //  Serial.println();
+
+  //Uncomment below to send converted values through serial connection.
   //  Serial.print(" Q1= ");
   //  Serial.print(double(Q1));
   //  Serial.print("      Q2= ");
@@ -137,61 +160,83 @@ int voltage2angle(int qin1, int qin2, int qin3)
   //  Serial.println(qS2);
   //  Serial.println();
   //  Serial.println();
-  //
-  //  //  Serial.println();
-  //  Serial.print("            time it took = ");
-  //  Serial.println(then - now);
-  //  Serial.println();
+
   //Serial.println("conversion complete...");
+
   return 1;
 }//voltage2angle
 
 
 //-----------------------------------------------------
+
+
 bool checksensors ()
 {
+  // This function checks the sensor on HUE connected to the arduino nano on defined pins.
+
+  // If the value of the parameters are outside defined limits,
+  //   a warning will be sent through the serial port and a false returned
+  // If the sensor value are within limits a value of true will be returned.
+
   //Serial.print("checking analog sensors...");
-  //  rA = analogRead(prA);
-  //  rB = analogRead(prB);
-  //  rC = analogRead(prC);
+  int rA = analogRead(prA);
+  int rB = analogRead(prB);
+  int rC = analogRead(prC);
+
   if (  (rA < rA_max) && (rB < rB_max) && (rC < rC_max) && (rA > rA_min) && (rB > rB_min) && (rC > rC_min))
   {
     //Serial.println("Sensors indicate HUE is ready to move...");
     return true;
-
   }
-  //Serial.println((rA > rA_min) && (rB > rB_min) && (rC > rC_min));
-  //delay(1000);
-  //Serial.println("WARNING: HUE OUTSIDE MECHANICAL LIMITS....");
-  return false;
+  else
+  {
+    Serial.println("WARNING: HUE OUTSIDE MECHANICAL LIMITS....");
+    return false;
+  }
 }//checksensors
 
-bool checkangles()
+//----------------------------
+
+
+bool checkangles(int Q1, int Q2, int Q3)
 {
+  //This function checks if the input angle is within the defined limits of HUE's range
+
+  //If the angle would be outside HUE's range a warning is sent through the serial port.
+  //If the angle is within limits a true value will be returned
   if (  Q1 < Q1_max && Q2 < Q2_max && Q3 < Q3_max && Q1 > Q1_min && Q2 > Q2_min && Q3 > Q3_min)
   {
     return true;
   }
-  return false;
+  else
+  {
+    Serial.println("WARNING: HUE CANNOT MOVE HERE....");
+    return false;
+  }
 }
+
+//------------------------------------------
 
 void displaysensordata()
 {
-  rA = analogRead(prA);
-  rB = analogRead(prB);
-  rC = analogRead(prC);
+  //This function sends current sensor data through the serial port
+  int  rA = analogRead(prA);
+  int  rB = analogRead(prB);
+  int  rC = analogRead(prC);
   Serial.print("      A= ");
   Serial.print(rA);
   Serial.print("      B= ");
   Serial.print(rB);
   Serial.print("      C= ");
   Serial.println(rC);
-
 }
 
-//-------------------------------------------------------------------------------------------------------------------------start
+
+//-------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
+  //Runs once upon boot of arduino
+
   //------------------------Set up steppers motors
   alpha.setEnablePin(MOTOR_X_ENABLE_PIN);
   alpha.setPinsInverted(false, false, false);
@@ -213,11 +258,15 @@ void setup()
   charlie.setMaxSpeed(charlie_speed);
 
   //---------------------------
-  //-----------------------board set up
+  //-----------------------Serial set up
   Serial.begin(9600);
   Serial.setTimeout(100);
-  s_input.reserve(20);
+  s_input.reserve(20); //Reserve data up to 10 character or integers
 
+
+  //define pin modes
+
+  //These are used by stepper objects
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -226,16 +275,20 @@ void setup()
   pinMode(7, OUTPUT);
   pinMode(8, OUTPUT);
 
+  //These are used by sensors
   pinMode(prA, INPUT);
   pinMode(prB, INPUT);
   pinMode(prC, INPUT);
+
+  //These are extra ihputs
   pinMode(A4, INPUT);
   pinMode(A3, INPUT);
+
+  // This enables the stepper motors. Set pin 8 high to disable
   digitalWrite(8, LOW);
 
   //home motors
-  //checksensor();
-  //int t = voltage2angle(rA,rB,rC);
+  //Set motors to home position: Make sure HUE starts from this defined position
   alpha.setCurrentPosition(3);
   beta.setCurrentPosition(14);
   charlie.setCurrentPosition(0);
@@ -244,39 +297,51 @@ void setup()
   //-----------------------------
 } //setup
 
+
+
+
+//***********************************MAIN PROGRAM
+
 void loop()
 {
-  bool howcopy = 0;
+  bool howcopy = 0;// a check variable used for return values of functions
+
   //displaysensordata();
-  delay(100);
+
+
   //read Serial Data if available in a string
   if (Serial.available())
   {
     //Serial.println("reading serial...");
-    //delay(1000);
     s_input = Serial.readStringUntil('\n');
 
     //parse the input string if it has content
     if (s_input.length() == 10)
     {
-      if (s_input[0] == 'p') {
-        a_s = (s_input.substring(1, 4)).toInt();
-        b_s = (s_input.substring(4, 7)).toInt();
-        c_s = (s_input.substring(7, 10)).toInt();
+      //call functions and tasks here based on the first character in string
 
-        howcopy = voltage2angle(a_s, b_s, c_s);
+      if (s_input[0] == 'p') {
+        //p indicated positional data. Seperate into 3 digit analog values
+        int a_s = (s_input.substring(1, 4)).toInt();
+        int b_s = (s_input.substring(4, 7)).toInt();
+        int c_s = (s_input.substring(7, 10)).toInt();
+        //Convert the analog voltage into step value
+        howcopy = voltage2angle(a_s, b_s, c_s);//This updates S1, S2 and S3 if howcopy==1;
       }//position
     } // if (s_input)
   }   //if
 
+
   if (howcopy) // ( checksensors() && howcopy)
   {
-  //  Serial.println("Setting destination...");
+    //if the positional data was valid and converted to step value successfully set targets for steppers.
+    
+    //  Serial.println("Setting destination...");
     //delay(1000);
     alpha.moveTo(S1);
     beta.moveTo(S2);
     charlie.moveTo(S3);
-    //
+    
     //    Serial.print("Current Position :");
     //    Serial.print("   ");
     //    Serial.print(alpha.currentPosition());
@@ -291,7 +356,6 @@ void loop()
     //    Serial.print(S2);
     //    Serial.print("   ");
     // Serial.println(S3);
-    //delay(1000);
   }//if within limits
   else
   {
@@ -299,14 +363,17 @@ void loop()
   }
   //run motors and check for more input
 
-  while ((alpha.isRunning() || beta.isRunning() || charlie.isRunning()) )//&& !Serial.available())
+  while ((alpha.isRunning() || beta.isRunning() || charlie.isRunning()) && !Serial.available())
   {
-    //    Serial.println("running");
-   // Serial.println("...stepping...");
+    //This will step motors 1 step each loop (if required by the moveTo property) until all motors have reached
+    // there destination.
+    //If more Serial data come in, stop moving and read the data.
+
+    // Serial.println("...stepping...");
+    //runs motor a maximum of 1 step if required
     alpha.run();
     beta.run();
     charlie.run();
-
   }//while
 
 } //loop
