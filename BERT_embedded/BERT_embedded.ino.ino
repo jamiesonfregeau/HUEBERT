d32038d9a57418f2d2c8691f64473214bcd20816
jamiesonfregeau <jamiesonjfregeau@gmail.com>
2021-11-18 16:09:39 -0800

    Ready for report

--- BERT_embedded/BERT_embedded.ino.ino
+++ BERT_embedded/BERT_embedded.ino.ino
@@ -1,6 +1,20 @@
+/*
+Control program for BERT to read positional sensor data, average it and send it to
+BERT's control system
+*/
+
+// Global constants
+// interval of time to average positional sensor values for
 unsigned long interval = 15;
-void setup() {
- // put your setup code here, to run once:
+char positional_character = "p";
+char final_character = "\n";
+
+void setup()
+{
+  // Set serial to a baud rate of 9600 and configure analog and digital pins
+  // Analog pins are for the potentiometers
+  // Digital pin is for the control button
+
   Serial.begin(9600);
   pinMode(A7, INPUT);
   pinMode(A6, INPUT);
@@ -8,15 +22,19 @@ void setup() {
   pinMode(2, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Instantiate local variables
   unsigned long previousMillis = millis();
   unsigned long currentMillis = millis();
   int n = 0;
   unsigned long int a = 0;
   unsigned long int b = 0;
   unsigned long int c = 0;
-  while ((currentMillis - previousMillis) < interval) {
+
+  // Average control values over period of time of interval
+  while ((currentMillis - previousMillis) < interval)
+  {
     a = analogRead(A7) + a;
     b = analogRead(A6) + b;
     c = analogRead(A5) + c;
@@ -24,31 +42,21 @@ void loop() {
     n = n + 1;
     currentMillis = millis();
   }
+
+  // Set control values to average over the time interval
   a = a / n;
   b = b / n;
   c = c / n;
+
+  // Reset time
   previousMillis = currentMillis;
-//  Serial.print("   number of samples= ");
-//  Serial.print(n);
 
-  String control_data = "p" + String(a) + String(b) + String(c)+ "\n";
-//  Serial.print(control_data);
-  
- if(digitalRead(2)){
+  // Set control data value
+  String control_data = positional_character + String(a) + String(b) + String(c) + final_character;
+
+  // Send control data to control system if BERT's control button is pressed
+  if (digitalRead(2))
+  {
     Serial.print(control_data);
-}
-//  
-
-//  a = analogRead(A7);
-//  b = analogRead(A6);
-//  c = analogRead(A5);
-//   
-//  Serial.print("      A= ");
-//  Serial.print(a);
-//  Serial.print("      B= ");
-//  Serial.print(b);
-//  Serial.print("      C= ");
-//  Serial.println(c);
-//  delay(10);
-
+  }
 }
